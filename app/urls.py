f541be0f2a159830d2d58fad0909b2db37663ce9
Boris Sulicenko <b.sulicenko@gmail.com>
2021-01-05 20:10:14 +0100

    Fix url order

--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     path('', views.home, name='app-home'),
     path('about/', views.about, name='app-about'),
     path('projects/', views.projects, name='app-projects'),
+    path('project/new/', views.ProjectCreateView.as_view(), name='project-create'),
     path('project/<pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
     path('project/<pk>/delete', views.ProjectDeleteView.as_view(), name='project-delete'),
-    path('project/new/', views.ProjectCreateView.as_view(), name='project-create'),
     
     path('project/<project_pk>/wiki/', views.wikis_list_view, name='wiki-list'),
     path('project/<project_pk>/wiki/new/', views.WikiCreateView.as_view(), name='wiki-create'),
