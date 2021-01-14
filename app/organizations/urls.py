bb6f533926d35e4c83f1f51fe30319f30ef57f1c
Boris Sulicenko <b.sulicenko@gmail.com>
2021-01-14 18:20:52 +0100

    Organizations: Update and delete organization

--- app/organizations/urls.py
+++ app/organizations/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('organizations/', views.AllOrganizationsListView.as_view(), name='organization-list-all'),
     path('organizations/new/', views.OrganizationCreateView.as_view(), name='organization-create'),
     path('organizations/<pk>/', views.OrganizationDetailView.as_view(), name='organization-detail'),
+    path('organizations/<pk>/edit/', views.OrganizationUpdateView.as_view(), name='organization-edit'),
+    path('organizations/<pk>/delete/', views.OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
--- app/organizations/views.py
+++ app/organizations/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Organization
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 class AllOrganizationsListView(ListView):
@@ -21,3 +21,18 @@ class OrganizationCreateView(LoginRequiredMixin, CreateView):
 class OrganizationDetailView(DetailView):
     model = Organization
     template_name = 'app/organization/organization_detail.html'
+
+class OrganizationUpdateView(LoginRequiredMixin, UpdateView):
+    model = Organization
+    template_name = 'app/organization/organization_update.html'
+    fields = ['name', 'description']
+
+    # TODO: dodati test_func tako da samo clanovi mogu da menjaju stvari
+
+class OrganizationDeleteView(LoginRequiredMixin, DeleteView):
+    model = Organization
+    template_name = 'app/organization/organization_confirm_delete.html'
+    # TODO: vidi u wiki kako si iskoristio reverse za ovo
+    success_url = '/organizations'
+
+    # TODO: dodati test_func za proveru da samo clanovi mogu da obrisu
