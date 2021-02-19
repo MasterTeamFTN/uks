ea18908a1e23d66372db850c8cf1ea63f6558171
Janko Ljubic <ljubicjanko1 AT gmail DOT com>
2021-02-20 00:00:04 +0100

    Static files replaced. Postgres database activated.

--- uks/settings.py
+++ uks/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'uks.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     # 'HOST': '127.0.0.1',
-    #     'HOST': 'db',
-    #     # 'HOST': 'localhost',
-    #     'PORT': 5432,
-    #     'OPTIONS': {
-    #         'options': '-c search_path=django,public'
-    #     },
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        # 'HOST': '127.0.0.1',
+        'HOST': 'db',
+        # 'HOST': 'localhost',
+        'PORT': 5432,
+        'OPTIONS': {
+            'options': '-c search_path=django,public'
+        },
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
